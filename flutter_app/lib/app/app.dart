b059778b6452a75b39e3334dee5266226395b0b9
phakphum12345 <166884793+phakphum12345@users.noreply.github.com>
2026-09-13 23:07:54 +0700

    Update app.dart

---
+++ flutter_app/lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'app_theme.dart';
+import 'navigation.dart';
+
+class DefensiveReferenceApp extends StatelessWidget {
+  const DefensiveReferenceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Defensive Device Service',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.dark(),
+      initialRoute: AppNavigation.homeRoute,
+      routes: AppNavigation.routes(),
+    );
+  }
+}
